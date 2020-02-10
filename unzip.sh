730aebcef3176770b5332e8bff80b27335b66630
vekio <albertocasta@protonmail.com>
2020-02-10 21:43:27 +0100

    Update unzip installer script

--- unzip.sh
+++ unzip.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#
+
 ##################################################################################################################
-# Version   :   1.0
-# Author 	:   vekio
-# Purpose   :   install unzip
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	install unzip
+# Comments	:	
 ##################################################################################################################
 
-sudo apt update && sudo apt install -y unzip
+# update the package lists
+sudo apt update
+
+# install unzip
+sudo apt install -y unzip
