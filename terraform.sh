12909a33d20008c5b20f0d43d74adf768d659c5d
vekio <albertocasta@protonmail.com>
2020-02-10 21:42:36 +0100

    Update terraform

--- terraform.sh
+++ terraform.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-#
+
 ##################################################################################################################
-# Version   :   1.0
-# Author 	:   vekio
-# Purpose   :   install terraform in linux 64-bit machine
-# Links     :   https://www.terraform.io/downloads.html
-# Comments	:	version of Terraform (0.12.17)
+# Version	:	1.1
+# Author	:	vekio
+# Purpose	:	install terraform (linux 64-bit)
+# Comments	:	Upgrade to version 0.12.20
+#               https://www.terraform.io/downloads.html
 ##################################################################################################################
 
+# download link
+LINK=https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
+
+# zip file name
+ZIP=$(echo "${LINK##*/}")
+
+# check for unzip
+command -v unzip >/dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Aborting."; exit 1; }
+
+# download terraform
 cd /tmp
-wget https://releases.hashicorp.com/terraform/0.12.17/terraform_0.12.17_linux_amd64.zip
-unzip terraform_0.12.17_linux_amd64.zip
+wget $ENLACE
+
+# install terraform
+unzip $ZIP
 sudo mv terraform /usr/local/bin/
-rm terraform_0.12.17_linux_amd64.zip
+
+# remove terraform.zip
+rm $ZiP
