5b3b0147fa4d0972b8a1d10b8867fccf3e6592e4
vekio <albertocasta@protonmail.com>
2020-02-12 16:51:51 +0100

    Add tmux installer script

---
+++ tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##################################################################################################################
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	install tmux
+# Comments	:	cheatsheet http://atkinsam.com/documents/tmux.pdf
+##################################################################################################################
+
+# update the package lists
+sudo apt update
+
+# install tmux
+sudo apt install -y tmux
