bc5ac7fd6762ad8b3c2c2f467011efae81ecd89b
vekio <albertocasta@protonmail.com>
2020-02-26 18:47:47 +0100

    Add fzf

---
+++ software/fzf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Name			:	fzf
+# Author		:	vekio
+# Description	:	install fzf, command line fuzzy finder
+# Notes			:	https://github.com/junegunn/fzf
+##################################################################################################################
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
